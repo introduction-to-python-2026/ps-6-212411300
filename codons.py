28c003a8debb698f03f9d309b64909391bea67c0
212411300 <racheliseitz1@gmail.com>
2025-11-30 16:04:15 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,7 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+    codonamino_dict = {}
+    with open(file_path) as file:
+        codonamino_dict = {row.strip().split('\t')[0]: row.strip().split('\t')[2] for row in file.readlines()}
+    return codonamino_dict # Replace the pass with your code
 
 
